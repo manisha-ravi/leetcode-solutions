0d2cdfb53b1fa1c582b2eaf912f3d5c935bf1dc2
manisha-ravi <manisharavi0621@gmail.com>
2026-03-28 17:34:39 +0530

    Update 175-combine-two-tables.sql

--- SQL/175-combine-two-tables.sql
+++ SQL/175-combine-two-tables.sql
@@ -1,3 +1,7 @@
+-- problem: Combine two tables
+-- problem no: 175
+--Link: https://leetcode.com/problems/combine-two-tables/
+--Approach: LEFT JOIN
 select firstName, lastName, city,state
 from Person
 left join Address
